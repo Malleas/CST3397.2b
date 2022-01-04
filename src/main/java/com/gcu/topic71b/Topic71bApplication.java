dfc41c3a3501b859ebb4ef506e265cd6424dd157
ntqams30 <msievers@thegeneral.com>
2022-01-04 08:28:10 -0600

    7.2b

--- src/main/java/com/gcu/topic71b/Topic71bApplication.java
+++ src/main/java/com/gcu/topic71b/Topic71bApplication.java
@@ -2,8 +2,10 @@ package com.gcu.topic71b;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Topic71bApplication {
 
     public static void main(String[] args) {
